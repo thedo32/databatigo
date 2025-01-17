44c60892fdeb67126be38da0ab2bcc0a64db480d
Jeffri <jeffriargon@hotmail.com>
2025-01-17 17:48:52 +0700

    update 2024-12-31

--- BatigoData.py
+++ BatigoData.py
@@ -23,18 +23,27 @@ Email: databaseoutlet@gmail.com
 )
 
 
-urluser = "https://restful.kopibatigo.id/users"
-urlhit = "https://restful.kopibatigo.id/hits"
-
 # CSS to customize the delta color for Streamlit's metric component
 st.markdown("""
     <style>
+    /* Import custom fonts */
+    @import url('https://fonts.googleapis.com/css2?family=Audiowide&family=Protest+Guerrilla&family=Tilt+Neon&display=swap');
+
+    /* Apply the custom font globally */
+    html, body, div, [class*="css"] {
+        font-family: 'Tilt Neon', sans-serif !important;
+    }
+
     /* Apply custom color to Streamlit metric delta */
     div[data-testid="stMetricDelta"] {
         color: #A7D129 !important;
     }
     </style>
     """, unsafe_allow_html=True)
+
+urluser = "https://restful.kopibatigo.id/users"
+urlhit = "https://restful.kopibatigo.id/hits"
+
 
 st.markdown("<h1 style='text-align: center;'>Analisa Data Kopi Batigo</h1>",unsafe_allow_html=True)
 
